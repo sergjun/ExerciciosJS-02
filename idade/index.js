a3197824bfc2492ae06d8cc9a4a7692f124f78a6
sergjun <serrgio.madureira@gmail.com>
2021-05-21 16:22:56 -0300

    refactoring so the function works with parameters and not with variables

--- idade/index.js
+++ idade/index.js
@@ -1,20 +1,20 @@
-const idade = { nome: "Sergio", anos: 28, meses: 7, dias: 200}
+// const idade = { nome: "Sergio", anos: 28, meses: 7, dias: 200}
 const ano = 365
 const mes = 30
 
 
 
-function calcIdade (){
+function calcIdade (nome="", anos=0, meses=0, dias=0){
 
     // console.log(idade.nome, idade.anos, idade.meses, idade.dias)
-    let mesesDias = idade.meses * mes
-    let anosDias = idade.anos * ano
-    let idadeDias = mesesDias + anosDias + idade.dias
+    let mesesDias = meses * mes
+    let anosDias = anos * ano
+    let idadeDias = mesesDias + anosDias + dias
     
-    return `${idade.nome} sua idade em dias é ${idadeDias}`
+    return `${nome} sua idade em dias é ${idadeDias}`
 
     
     
 }
 
-console.log(calcIdade ())
+console.log(calcIdade ("Sergio",28 , 7 ,200 ))
